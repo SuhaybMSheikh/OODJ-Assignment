030c5677eb9ab2e462ec0f4e3d5aeaa4c99a2687
JimNoobZ <yeapzhenghui@gmail.com>
2026-04-04 15:41:07 +0800

    test_commit4

--- src/main/LoginFrame.java
+++ src/main/LoginFrame.java
@@ -69,7 +69,7 @@ public class LoginFrame extends JFrame {
 
         buildUI();
     }
-
+    im gay
     private void buildUI() {
         cardLayout = new CardLayout();
         mainPanel  = new JPanel(cardLayout);
